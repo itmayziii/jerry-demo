8aafeefafc038b537e60450a7fd869a60cb2cc4c
Tommy May <tommymay37@gmail.com>
2022-12-22 21:47:50 -0500

    added hello jerry

--- src/App.js
+++ src/App.js
@@ -7,8 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. hello jerry
         </p>
+        {blah.map((b) => {
+          return (<p>{b}</p>)
+        })}
         <a
           className="App-link"
           href="https://reactjs.org"
